24274ad63c2d6e6c4577b2713e282345282a6a9a
Fiido <roccobosio@gmail.com>
2024-03-17 17:22:55 -0300

    connect posts microservice

--- apps/posts/src/posts.module.ts
+++ apps/posts/src/posts.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { CommonService } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService, CommonService],
 })
 export class PostsModule {}
