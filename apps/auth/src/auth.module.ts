9c14d3cfd04c138659e5c4c73096dddeb2796b74
Fiido <roccobosio@gmail.com>
2024-03-17 17:22:33 -0300

    connect auth microservice

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { CommonService, DatabaseModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_URI: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, CommonService],
 })
 export class AuthModule {}
