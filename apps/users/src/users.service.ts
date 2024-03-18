d315066e257eb6b5d2d640764ee110505220e932
Fiido <roccobosio@gmail.com>
2024-03-17 22:37:19 -0300

    user consumer

--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { UpdateUserDto, UserRepository } from '@app/common';
+import { User } from '@app/common/entities/user.entity';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private userRepository: UserRepository) {}
+
+  async findUsers(): Promise<User[]> {
+    return this.userRepository.findAll();
+  }
+
+  async findUser(id: number): Promise<User> {
+    const userFounded = await this.userRepository.findOneById(id);
+    if (!userFounded)
+      throw new RpcException({
+        message: 'user not found',
+        status: HttpStatus.NOT_FOUND,
+      });
+
+    return userFounded;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const userFounded = await this.findUser(id);
+
+    const userUpdated = Object.assign(userFounded, updateUserDto);
+
+    return this.userRepository.save(userUpdated);
+  }
+
+  async delete(id: number): Promise<User> {
+    const userFounded = await this.findUser(id);
+    await this.userRepository.delete(id);
+
+    return userFounded;
   }
 }
