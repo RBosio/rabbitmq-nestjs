fc43a5de7d3b9b50ac3311c3c2ef704a3ae3699c
Fiido <roccobosio@gmail.com>
2024-03-18 23:06:21 -0300

    post endpoints

--- apps/api-gateway/src/posts/posts.controller.ts
+++ apps/api-gateway/src/posts/posts.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Inject,
   NotFoundException,
   Param,
@@ -15,18 +16,52 @@ import { ClientRMQ } from '@nestjs/microservices';
 import { catchError, of } from 'rxjs';
 import { CreatePostDto, UpdatePostDto } from '@app/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import {
+  ApiBody,
+  ApiCookieAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('post')
+@ApiResponse({
+  status: HttpStatus.UNAUTHORIZED,
+  description: 'unauthorized',
+})
+@ApiCookieAuth()
 @UseGuards(AuthGuard)
 @Controller('post')
 export class PostsController {
   constructor(@Inject(POST_SERVICE) private postClient: ClientRMQ) {}
 
   @Get()
+  @ApiOperation({ summary: 'find all posts' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'get posts',
+  })
   findPosts() {
     return this.postClient.send({ cmd: 'findPosts' }, {});
   }
 
   @Get(':postId')
+  @ApiOperation({ summary: 'find one post' })
+  @ApiParam({
+    name: 'postId',
+    type: 'number',
+    example: 1,
+    required: true,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'get post',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'post not found',
+  })
   findPost(@Param('postId') postId: number) {
     return this.postClient.send({ cmd: 'findPost' }, postId).pipe(
       catchError((val) => {
@@ -40,6 +75,16 @@ export class PostsController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'create post' })
+  @ApiBody({ type: CreatePostDto })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'post created',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'user not found',
+  })
   createPost(@Body() createPostDto: CreatePostDto) {
     return this.postClient.send({ cmd: 'createPost' }, createPostDto).pipe(
       catchError((val) => {
@@ -53,6 +98,22 @@ export class PostsController {
   }
 
   @Patch(':postId')
+  @ApiOperation({ summary: 'update post' })
+  @ApiParam({
+    name: 'postId',
+    type: 'number',
+    example: 1,
+    required: true,
+  })
+  @ApiBody({ type: UpdatePostDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'post updated',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'post not found',
+  })
   updatePost(
     @Param('postId') postId: number,
     @Body() updatePostDto: UpdatePostDto,
@@ -71,6 +132,21 @@ export class PostsController {
   }
 
   @Delete(':postId')
+  @ApiOperation({ summary: 'delete post' })
+  @ApiParam({
+    name: 'postId',
+    type: 'number',
+    example: 1,
+    required: true,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'post deleted',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'post not found',
+  })
   deletePost(@Param('postId') postId: number) {
     return this.postClient.send({ cmd: 'deletePost' }, postId).pipe(
       catchError((val) => {
