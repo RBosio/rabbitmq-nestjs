f9235b9773fc36b69c63223cfabcf80b2f36d4b0
Fiido <roccobosio@gmail.com>
2024-03-18 22:52:31 -0300

    post dto

--- libs/common/src/dto/post/create-post.dto.ts
+++ libs/common/src/dto/post/create-post.dto.ts
@@ -1,14 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({
+    name: 'title',
+    type: 'string',
+    example: 'A simple title',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   title: string;
 
+  @ApiProperty({
+    name: 'description',
+    type: 'string',
+    example: 'A simple description',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   description: string;
 
+  @ApiProperty({
+    name: 'userId',
+    type: 'number',
+    example: 1,
+    required: true,
+  })
   @IsNotEmpty()
   @IsNumber()
   userId: number;
